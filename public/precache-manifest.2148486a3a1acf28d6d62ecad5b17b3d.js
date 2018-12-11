76f9f2068b80aa74d88fef9720f35e739ee685fd
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2018-12-11 16:49:23 +0100

    Add first React files

---
+++ public/precache-manifest.2148486a3a1acf28d6d62ecad5b17b3d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "277b66f7132267a2da20",
+    "url": "/static/js/main.277b66f7.chunk.js"
+  },
+  {
+    "revision": "b015d4e366eaa55fd687",
+    "url": "/static/js/1.b015d4e3.chunk.js"
+  },
+  {
+    "revision": "277b66f7132267a2da20",
+    "url": "/static/css/main.3f665134.chunk.css"
+  },
+  {
+    "revision": "a118fd31cd5e7e0b7ca4e37ca494b6e0",
+    "url": "/index.html"
+  }
+];
